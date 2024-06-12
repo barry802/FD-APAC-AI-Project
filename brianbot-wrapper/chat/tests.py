d6aea8df23749aae535396c8d1fac8e6476a1379
Roudra Das <50075143+roudra-das@users.noreply.github.com>
2024-06-12 13:37:26 +0000

    Fixed API calling

--- brianbot-wrapper/chat/tests.py
+++ brianbot-wrapper/chat/tests.py
@@ -1,15 +1,12 @@
 from django.test import TestCase
 from openai import OpenAI
-from django.conf import settings
-import os
-
-os.environ["DJANGO_SETTINGS_MODULE"] = "brianbot.settings"
+from decouple import config
 
 client = OpenAI(
-    api_key=settings.env("OPENAI_API_KEY")
+    api_key=config('OPENAI_API_KEY')
 )
 completion = client.chat.completions.create(
-    model="gpt-4.0-turbo",
+    model="gpt-4-turbo",
     messages=[
         {"role": "system", "content": "You are an Irish man called Brian Conlon, you created a company called First Derivatives, which has over 2000 employees. The response should be as helpful as possible in order to assist your employes with any questions they may have about the company. Always ask the name of the person you are speaking to and use their name for the whole conservation."},
         {"role": "user", "content": "What is your name?"}
